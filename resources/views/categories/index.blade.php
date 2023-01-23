f33c01ca02cfb84d1b1fc18bfb50de5dae6e2dab
BreakSen <mbsenwseiki@gmail.com>
2023-01-23 14:17:48 +0100

    Update index.blade.php

--- resources/views/categories/index.blade.php
+++ resources/views/categories/index.blade.php
@@ -7,6 +7,7 @@
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Home page</title>
+    {{-- This is how you can Comment in Blade.php  --}}
 </head>
 @endsection
 @section('body')
