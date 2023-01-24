a5eb80062a26d31a63f41d5bf9acf3907def4e04
BreakSen <mbsenwseiki@gmail.com>
2023-01-24 11:21:24 +0100

    breeze --dev

--- resources/views/categories/index.blade.php
+++ resources/views/categories/index.blade.php
@@ -1,4 +1,4 @@
-<x-app-layout>
+{{-- <x-app-layout>
     <x-slot name="header">
         <h2 class="font-semibold text-xl text-gray-800 leading-tight">
             {{ __('Book List') }}
@@ -32,7 +32,7 @@
             @endforeach
         </div>
     </div>
-</x-app-layout>
+</x-app-layout>--}}
 
 <!DOCTYPE html>
 <html lang="en">
